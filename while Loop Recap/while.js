ed11ca21a81ea54b43def53ea7d687b62c6fbd83
COMRADE040 <rafiulislam040@gmail.com>
2025-09-15 23:33:33 +0600

    easiest problem

---
+++ while Loop Recap/while.js
@@ -0,0 +1,44 @@
+
+// While Loop 
+
+let i = 1; // Initial Value of outside while
+let text = 1
+
+// // inside third bracket condition
+// while (i < 10) { // Loop runs until i reaches 10
+//     text += "The Number is " + i + "<br/>"; // make <br/> a string
+//     i++; // 1 by 1 increase
+// }
+// // console.log(text)
+// document.write(text);
+
+
+
+
+// while(i <= 10){
+//     if(i % 2 === 0){
+//         // i += 2
+//         console.log("Even " + i)
+//     }
+//     i++
+// }
+
+// document.write(text)
+
+
+
+// Sum of first 5 natural numbers (1 + 2 + 3 + 4 + 5)
+// while(i <= 5){
+//     text += i
+//     i++
+//     console.log(text)
+// }
+
+// Multiply numbers from 1 to 5 (factorial of 5)
+
+
+while(i <= 5){
+    text = '5' + " * " + i + " = " + i * 5
+    i++;
+    console.log(text)
+}
